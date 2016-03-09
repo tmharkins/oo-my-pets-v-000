940c00ce630c20940613b5a1c6e5b6d0fa4db79f
tmharkins <tmharkins@gmail.com>
2016-03-09 00:24:58 -0600

    finished my pets lab

--- lib/fish.rb
+++ lib/fish.rb
@@ -1,3 +1,13 @@
+require 'pry'
 class Fish
   # code goes here
+# binding.pry
+  attr_accessor :mood
+  attr_reader :name
+
+  def initialize(name)
+  	@name = name
+  	@mood = "nervous"
+  	Owner.all << self
+  end
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,67 @@
+require 'pry'
 class Owner
-  # code goes here
+
+  attr_accessor :all, :name, :pets, :mood
+  attr_reader :species
+  @@all = []
+  def initialize(species)
+  	@pets = {fishes: [], cats: [], dogs: []}
+  	@species = species
+  	@@all << self
+  end
+
+  def self.all
+  	@@all
+  end
+
+  def self.reset_all
+  	all.clear
+  end
+
+  def self.count
+  	all.size
+  end
+
+  def say_species
+  	"I am a #{@species}."
+  end
+
+  def buy_fish(name)
+  	pets[:fishes] << Fish.new(name)
+  end
+
+  def buy_cat(name)
+  	pets[:cats] << Cat.new(name)
+  end
+  
+  def buy_dog(name)
+  	pets[:dogs] << Dog.new(name)
+  end
+
+  def walk_dogs
+  	pets[:dogs][0].mood = "happy"
+  end
+
+  def play_with_cats
+  	pets[:cats][0].mood = "happy"
+  end
+
+  def feed_fish
+  	pets[:fishes][0].mood = "happy"
+  end
+
+  def sell_pets
+  	pets.clear
+  	Owner.all.each do |each_pet|
+			each_pet.mood = "nervous" if each_pet.mood != nil
+  	end
+  	Owner.reset_all
+  end
+
+  def list_pets
+	  fish_count = Owner.all[0].pets[:fishes].size
+	  dog_count = Owner.all[0].pets[:dogs].size
+	  cat_count = Owner.all[0].pets[:cats].size
+	  "I have #{fish_count} fish, #{dog_count} dog(s), and #{cat_count} cat(s)."
+	end
 end
